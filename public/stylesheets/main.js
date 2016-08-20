d1eaf59b23be7fbfe1e437b71b6ee0e6fbb883c1
Joshua Lovell <joshualylelovell@gmail.com>
2016-08-20 12:59:48 -0400

    some style changes. made progress on displaying ingredient list as <ul>

--- public/stylesheets/main.js
+++ public/stylesheets/main.js
@@ -6,8 +6,46 @@ function splitString(stringToSplit, separator) {
   console.log('The array has ' + arrayOfStrings.length + ' elements: ' + arrayOfStrings.join(' || '));
 }
 
-var ingredientString = '3 eggs, dash of milk, salt and pepper to taste, Butter for pan';
+var ingredientString = '3 eggs, dash of milk, salt and pepper to taste, butter for pan';
 
 var comma = ',';
 
 splitString(ingredientString, comma);
+
+//one idea for appending to an <ul>
+
+var indexNum = 0;
+var unorderedList = document.getElementById('itemList');
+var ingredients = new Array("3 eggs", "dash of milk", "salt and pepper to taste", "butter for the pan");
+var myElement;
+
+function writeIngredients(){
+
+  for (var i = 0; i < ingredients.length; i++ ) {
+        // Create the <LI> element
+        myElement = document.createElement("LI");
+        // Add the letter between the <LI> tags
+        myElement.innerHTML = ingredients[indexNum++];
+        // Append the <LI> to the bottom of the <UL> element
+        unorderedList.appendChild(myElement);
+        }
+    }
+
+ writeIngredients();
+
+ //second idea:
+
+ var ingredients = new Array("2-3 eggs", "dash of milk", "salt and pepper to taste", "butter for the pan");
+
+ function writeIngredients(){
+     var items = document.getElementById("itemList");
+
+     for (var i = 0; i < ingredients.length; i++ ) {
+         var item = document.createElement("li");
+         item.innerHTML = ingredients[i];
+         items.appendChild(item);
+     }
+
+ }
+
+ writeIngredients();
